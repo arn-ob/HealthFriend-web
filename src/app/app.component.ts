c24a1c4c5d74aaaf252495d0b07bb1b71154e1c4
Arnob Almazee <arnobxtreme@gmail.com>
2019-02-08 02:13:19 +0600

    final Build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HealthFriend';
   sign = false;
+
+  ngOnInit() {
+    const a = localStorage.getItem('type');
+    if (a !== null) {
+      this.sign = true;
+    }
+  }
 }
